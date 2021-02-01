bf021f73b39d6bc50de54791a768cb1616ca5160
Colin Warner <colin.x.warner@gmail.com>
2021-02-01 16:46:28 -0600

    Updating

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 "use strict"
 
-//BELOW IS FROM EXISTING CODE. MOVING ALL VARS (GLOBAL) UP TOP FOR READABILITY? I DON"T KNOW. IT MADE SENSE AT THE TIME!!
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-//VAR COFFEES ARRAY OF OBJECTS WITH PROPERTIES OF ID, NAME, ROAST
+// VAR COFFEES IS AN ARRAY OF OBJECTS WITH PROPERTIES OF ID, NAME, ROAST
 var coffees = [
     {id: 1, name: 'Light City', roast: 'light'},
     {id: 2, name: 'Half City', roast: 'light'},
@@ -21,18 +20,14 @@ var coffees = [
 ];
 
 //THE BELOW QUERYSELECTORS ARE GRABBING THE FIRST ELEMENT IN THE DOCUMENT FROM THE ID(#)
-
 var coffeeList = document.querySelector('#coffees');
 var searchBtn = document.querySelector('#search-btn');
 var roastSelection = document.querySelector('#roast-selection');
-var search = document.querySelector('#search')
-//var dropdown = document.querySelector('#dropdown')
+var search = document.querySelector('#search');
+//var addRoast = document.querySelector('add-roast');
+//var addBtn = document.querySelector('#add-btn');
 
 // renderCoffees(coffees);
-
-
-
-
 
 // COFFEES//
 /*
@@ -53,15 +48,12 @@ function renderCoffee(coffee) {    //classList
     div.appendChild(coffeeRoast);
     return div;
 }
-
  */
-//STRING LITERAL ' ' is the string text. $ is the start of the expression.
+// STRING LITERAL ' ' IS THE STRING TEXT. $ IS THE START OF THE EXPRESSION.
 function renderCoffee(coffee) {
-    return `<h3>${coffee.name}</h3> <!-- I SEE THIS IN THE BROWSER BUT WHY NOT IN THE ACTUAL HTML??? -->
+    return `<h3>${coffee.name}</h3> 
             <p>${coffee.roast}</p>`
 }
-
-
 // function renderCoffees(coffees) {
 //   /*  coffees = coffees.sort((a,b) => {return b.id - a.id;});
 //     console.log(coffees); */
@@ -71,20 +63,16 @@ function renderCoffee(coffee) {
 //     }
 //     return html;
 // }
-
-
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
-//REDUCE - The reduce() method executes a reducer function (that you provide) on each element of the array, resulting in single output value.
-//STRINGBUILDER - StringBuilder objects are like String objects, except that they can be modified.
-//???IS THIS CREATING coffee?
+// REDUCE - The reduce() method executes a reducer function (that you provide) on each element of the array, resulting in single output value.
+// STRINGBUILDER - StringBuilder objects are like String objects, except that they can be modified. (NOTE STRINGBUILDER IS A JAVA CONCEPT THAT WE ARE MIMICKING HERE IN JS)
 function renderCoffees(coffees) {
    const coffeesHtml = coffees.reduce((stringBuilder, coffee) => {
         return stringBuilder.concat(renderCoffee(coffee))
     }, '')
     return coffeesHtml;
 }
-
-//innerHTML property sets or returns the HTML content (inner HTML) of an element.
+// innerHTML property sets or returns the HTML content (inner HTML) of an element.
 function updateCoffees(e) {   //same format for searching for name according to Zach
     e.preventDefault(); // don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
@@ -95,7 +83,6 @@ function updateCoffees(e) {   //same format for searching for name according to 
             filteredCoffees = filterByRoast(selectedRoast.toLowerCase())
         }
     coffeeList.innerHTML = renderCoffees(filteredCoffees);
-
 }
 //The filter() method creates an array filled with all array elements that pass a test (provided as a function).
 function filterByName(value) {
@@ -109,7 +96,6 @@ function filterByRoast(value) {
         const coffeeResult = coffeeRoast.search(selectedRoast)
         return coffeeResult > -1
     })
-
 }
 //e = event object being passed
 //e.target = The target event property returns the element that triggered the event.
@@ -120,13 +106,11 @@ function searchQuery(e) {
     const searchString = e.target.value;
     coffeeList.innerHTML = renderCoffees(filterByName(searchString))
 }
-
-
 // console.log(coffeeList);
 coffeeList.innerHTML = renderCoffees(coffees)
 searchBtn.addEventListener('click', updateCoffees);
 search.addEventListener('keyup', searchQuery);
-addedCoffee.addEventListener('click' , )
+//addedCoffee.addEventListener('click' , )
 // FILTER ROAST SEARCH ///
 // WHEN ROAST IS SELECTED, WE NEED ONLY THE COFFEES IN THAT ROAST TO DISPLAY ON THE LEFT W/O HITTING SUBMIT
 //
@@ -139,39 +123,26 @@ const dropDown = document.getElementById('roast-selection');
 dropDown.addEventListener('change', searchRoast)
     //console.log(event.target.value)
 
+// =================SECOND COFFEE SELECTION============================== //
+var addRoast = document.querySelector('add-roast');
+var addBtn = document.querySelector('#add-btn');
 
+function addCoffees(e) {   //same format for searching for name according to Zach
+    e.preventDefault();
+    var addedRoast = addRoast
+    var filteredCoffees;
+        if (addedRoast === 'All') {
+        filteredCoffees = coffees;
+    }else {
+        filteredCoffees = filterByRoast(addedRoast.toLowerCase())
+}
+coffeeList.innerHTML = renderCoffees(filteredCoffees);
 
-
-
-console.log('you found it! Enter CODE:TimeVortex in your cart to unlock our secret roast')
-
-
-//          START AUTOCOMPLETE CODE   --- STILL WORKING ON MAKING THE SECOND TEXT INPUT SEARCHABLE ---
-/*
-function searchBar() {
-
-    var input, filter, tbody, div, txtValue;
-    input = document.getElementById('search');
-    filter = input.value.toUpperCase();
-    tbody = document.getElementById("coffees");
-    div = tbody.getElementsByTagName('div');   //TBODY?
-
-
-    // FOR LOOP TO SORT THROUGH ITEMS IN AUTOCOMPLETE
-
-    for (var i = 0; i < div.length; i++) {
-        txtValue = div[i].textContent || div[i].innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            div[i].style.display = "";
-        } else {
-            div[i].style.display = "none";
-        }
-    }
 }
 
-*/
+// HIDDEN MESSAGE IN CONSOLE //
+console.log('you found it! Enter CODE:TimeVortex in your cart to unlock our secret roast')
 
-//           END AUTOCOMPLETE
 
 /*var coffeeName = document.getElementById('coffeeName').value;
 
